36e538272234f0ddcb95eb50c344820e4dc939b9
Adrian9777 <adrian.scheele@yahoo.com>
2026-02-10 08:31:44 +0100

    firstLayout

--- welt-des-webdesigns/src/App.jsx
+++ welt-des-webdesigns/src/App.jsx
@@ -2,7 +2,29 @@ import './App.css'
 
 function App() {
 
-  return (<h1>Test</h1>)
+  return (
+      <div className="app-layout">
+        <header className="header">
+          <h2>Wlt des Webdesigns</h2>
+        </header>
+
+        <aside className="navbar">
+          <h3>html</h3>
+          <button className="nav-btn">Grundlagen</button>
+          <button className="nav-btn">Fortgeschritten</button>
+          <button className="nav-btn">SEO</button>
+        </aside>
+
+        <main className="main">
+          <h1>Start</h1>
+          <p>fwnrngc wel nghluiglielwerg</p>
+        </main>
+
+        <aside className="sidebar">
+          <h2>Werbung</h2>
+        </aside>
+      </div>
+  )
 }
 
 export default App
